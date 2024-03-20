c10d55f1a3c2e4bc03d2b031f4c12fac21255bc1
bada308 <203190@jnu.ac.kr>
2024-03-20 11:11:31 +0900

    feat: 습관 생성 구현

--- static/js/habit.js
+++ static/js/habit.js
@@ -22,3 +22,66 @@ function handleCountUp(habitId) {
     },
   });
 }
+
+function handleCreateHabit() {
+  if (!confirm("습관을 추가하시겠습니까?")) {
+    return;
+  }
+
+  const habitName = $("#habitName").val();
+  const habitGoal = $("#habitGoal").val();
+  const habitTag = $("#habitTag").val();
+
+  console.log(habitName, habitGoal, habitTag);
+
+  if (!habitName) {
+    alert("습관 이름을 입력해주세요!");
+    return;
+  }
+
+  if (!habitGoal) {
+    alert("목표 횟수를 입력해주세요!");
+    return;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: `/habits`,
+    data: {
+      name: habitName,
+      goal: Number(habitGoal),
+      tag: habitTag,
+    },
+    success: function (response) {
+      if (response.result === "success") {
+        location.reload();
+      }
+    },
+  });
+}
+
+function handleCancelCreate() {
+  if (!confirm("습관 추가를 취소하시겠습니까?")) {
+    return;
+  }
+  $("#habitCreate").toggle();
+  $("#habitAddToggleText").text("추가하기");
+  $("#habitName").val("");
+  $("#habitGoal").val("");
+  $("#habitTag").val("");
+}
+
+function handleDeleteHabit(habitId) {
+  if (!confirm("습관을 삭제하시겠습니까?")) {
+    return;
+  }
+  $.ajax({
+    url: `/habits/${habitId}`,
+    method: "POST",
+    success: function (response) {
+      if (response.result === "success") {
+        location.reload();
+      }
+    },
+  });
+}
